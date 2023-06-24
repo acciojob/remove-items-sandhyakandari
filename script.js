30a00af7dcb8208b9cdc3a5b155f9648175cfceb
sandhya <114242237+sandhyakandari@users.noreply.github.com>
2023-06-24 07:20:07 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,8 @@
 //your JS code here. If required.
+function countop(){
+    const sele=document.getElementById("colorSelect");
+    sele.remove(sele.selectedIndex);
+    
+   //const val= sele.options[sele.selectedIndex].text;  
+  // console.log(val);
+}
